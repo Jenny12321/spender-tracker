72ba604da976571dea388f780d88d3c87d779361
Jenny Wang <j822wang@uwaterloo.ca>
2020-05-18 14:51:10 -0400

    Updates

---
+++ precache-manifest.38e7495b2f10fdb90b39d301c5e7ad56.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599681f2f2b99035839b38295bdb671c",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/index.html"
+  },
+  {
+    "revision": "be8c6a3adf37c897343d",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3040d3126455ec029497",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/css/main.01c8b783.chunk.css"
+  },
+  {
+    "revision": "be8c6a3adf37c897343d",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/js/2.b0317969.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/js/2.b0317969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3040d3126455ec029497",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/js/main.be170f81.chunk.js"
+  },
+  {
+    "revision": "39537e51a53575c5fd7a",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/js/runtime-main.3efd607f.js"
+  },
+  {
+    "revision": "44030cf35126bc2fe77f49b068fcef6a",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/media/business.44030cf3.png"
+  },
+  {
+    "revision": "17dd6d616b3a346264df4cafccbca01b",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/media/chef.17dd6d61.png"
+  },
+  {
+    "revision": "0820dd1eafc50bb610f38bd85ad64647",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/media/cover.0820dd1e.png"
+  },
+  {
+    "revision": "74a55bf86d4869ccdc99b83f050e5fcd",
+    "url": "/Jenny12321/order-out-tracker/tree/master/web-client/static/media/junk-food.74a55bf8.png"
+  }
+]);
